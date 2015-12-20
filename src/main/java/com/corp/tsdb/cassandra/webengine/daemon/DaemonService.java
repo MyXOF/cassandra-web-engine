b6f5fe44eab65a23cdfa7442e53b5fe541078523
MyXOF <xuyi556677@163.com>
2015-12-20 15:16:32 +0800

    add git ignore

--- src/main/java/com/corp/tsdb/cassandra/webengine/daemon/DaemonService.java
+++ src/main/java/com/corp/tsdb/cassandra/webengine/daemon/DaemonService.java
@@ -2,8 +2,13 @@ package com.corp.tsdb.cassandra.webengine.daemon;
 
 import net.sf.json.JSONArray;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.corp.tsdb.cassandra.webengine.config.Config;
 
 public class DaemonService {
 	private static final Logger logger = LoggerFactory.getLogger(DaemonService.class);
@@ -14,14 +19,36 @@ public class DaemonService {
 	
 	public JSONArray queryContent(String message){
 		JSONArray result = new JSONArray();
-		String values[] = message.split("\\s");
-		
-		if(values == null || values.length == 0){
+
+		Set<String> queryWords = filterMessage(message);
+		if(queryWords == null || queryWords.size() == 0){
 			return result;
 		}
 		
 		
+		
+		
 		return result;
+	}
+	
+	private Set<String> filterMessage(String message){
+		String values[] = message.split("\\s");
+		Set<String> stopWords = Config.getInstance().stopWords;
+		Set<String> queryWords = new HashSet<String>();
+		
+		if(values == null || values.length == 0){
+			return queryWords;
+		}
+		
+		for(String word : values){
+			String _word = word.toLowerCase();
+			if(stopWords.contains(_word)){
+				continue;
+			}
+			queryWords.add(_word);
+		}
+		
+		return queryWords;
 	}
 	
 	public static void main(String[] args) {
